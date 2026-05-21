21898b5cfb31390d30adb36a3b4e81049a0a88a4
Arpan Sarkar <arpansarkar112@gmail.com>
2026-05-21 21:53:44 +0300

    feat: add application and user models for MongoDB

---
+++ Project/backend/models/applicationModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const applicationSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['Pending', 'Interview', 'Rejected', 'Offer'],
+      default: 'Pending',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Application = mongoose.model('Application', applicationSchema);
+
+export default Application;
